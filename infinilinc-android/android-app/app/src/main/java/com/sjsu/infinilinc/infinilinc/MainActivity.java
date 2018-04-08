425dc28f99c426d926ca31e5d45458522b4e848c
Conner Short <conner.j.short@gmail.com>
2018-04-07 19:41:06 -0700

    Implemented Internet connectivity awareness.

--- infinilinc-android/android-app/app/src/main/java/com/sjsu/infinilinc/infinilinc/MainActivity.java
+++ infinilinc-android/android-app/app/src/main/java/com/sjsu/infinilinc/infinilinc/MainActivity.java
@@ -1,11 +1,18 @@
 package com.sjsu.infinilinc.infinilinc;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.webkit.ConsoleMessage;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
@@ -20,6 +27,9 @@ public class MainActivity extends NfcActivity {
 
     private String appDomain = "infinilinc.firebaseapp.com";
     private String appPath = "/debug.html";
+
+    private View mainLayoutView;
+    private View offlineLayoutView;
 
     @Override
     void onConnect() {
@@ -70,13 +80,37 @@ public class MainActivity extends NfcActivity {
         }
     }
 
+    void onNetStateChange(boolean connected) {
+        if(connected) {
+            setContentView(mainLayoutView);
+            mainWebView.loadUrl("https://" + appDomain + appPath);
+        } else {
+            setContentView(offlineLayoutView);
+            mainWebView.loadUrl("about:blank");
+
+            disable(); /* Disable NFC */
+        }
+    }
+
+    boolean isNetConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager)context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        return ((netInfo != null) && (netInfo.isConnectedOrConnecting()));
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        LayoutInflater inflater = getLayoutInflater();
 
-        mainWebView = (WebView)findViewById(R.id.main_web_view);
+        mainLayoutView = inflater.inflate(R.layout.activity_main, null);
+        offlineLayoutView = inflater.inflate(R.layout.offline, null);
+
+        mainWebView = mainLayoutView.findViewById(R.id.main_web_view);
 
         mainWebView.setWebViewClient(new WebViewClient() {
             @Override
@@ -116,7 +150,27 @@ public class MainActivity extends NfcActivity {
 
         mainWebView.addJavascriptInterface(this, "nfc");
 
-        mainWebView.loadUrl("https://" + appDomain + appPath);
+        /* Do an initial check on the network state before setting up the broadcast receiver */
+        onNetStateChange(isNetConnected(this));
+
+        /* Broadcast receiver to handle changes in network state */
+        BroadcastReceiver br = new BroadcastReceiver() {
+            @Override
+            public void onReceive(final Context context, Intent intent) {
+                if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            onNetStateChange(isNetConnected(context));
+                        }
+                    });
+                }
+            }
+        };
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+
+        registerReceiver(br, filter);
     }
 
     @Override
